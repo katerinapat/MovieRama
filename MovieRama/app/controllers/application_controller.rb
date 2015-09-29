b6b182b9578d48141566ba165bf84a9bad79d532
katerinapat <katerina.patticha@gmail.com>
2015-09-29 21:45:54 +0300

    Adding extra registration fields #1

--- MovieRama/app/controllers/application_controller.rb
+++ MovieRama/app/controllers/application_controller.rb
@@ -9,8 +9,7 @@ class ApplicationController < ActionController::Base
 
 		def configure_permitted_parameters
 			devise_parameter_sanitizer.for(:sign_up) do |u|
-  		  u.permit(:first_name, :last_name)
+  		  u.permit(:first_name, :last_name, :email, :password)
   		end
- # << :first_name :last_name
 		end
 end
